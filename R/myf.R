0fa8ca749ad49f360d700f68ce1760a1d40e9840
Reese <reese.mishler@ou.edu>
2021-06-08 15:02:47 -0500

    Initial file list

---
+++ R/myf.R
@@ -0,0 +1,14 @@
+#' Square function
+#'
+#' Takes  vector and returns a vectorof squared components
+#'
+#' @param x
+#'
+#' @return a vector of squared components
+#' @export
+#'
+#' @examples
+#' x=1:30; myf(x)
+myf=function(x){
+  x^2
+}
